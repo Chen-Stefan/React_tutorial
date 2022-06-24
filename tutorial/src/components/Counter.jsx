aca2700c7b9d8592518260adbdf0824de5a94484
Stefan Chen <zchen158@my.bcit.ca>
2022-06-24 12:35:20 -0700

    Conditional render using &&

--- tutorial/src/components/Counter.jsx
+++ tutorial/src/components/Counter.jsx
@@ -1,33 +1,31 @@
 import React, { Component } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css'
-import {Button, Badge} from 'react-bootstrap'
+import "bootstrap/dist/css/bootstrap.min.css";
+// import { Button, Badge } from "react-bootstrap";
 
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ['tag1', 'tag2', 'tag3']
+    tags: [],
   };
-  render() {
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
     return (
-      <div>
-        <span className= {this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-            {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-        </ul>
-      </div>
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     );
   }
 
-  getBadgeClasses() {
-    let classes = 'badge m-2 ';
-    classes += this.state.count === 0 ? 'bg-warning' : 'bg-primary';
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? 'Zero' : count;
+  render() {
+    return (
+      <div>
+        {this.state.tags.length === 0 && 'Please Create a new tag'}
+        {this.renderTags()}
+      </div>
+    );
   }
 }
 
