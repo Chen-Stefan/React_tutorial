ff06ced836de89cc82861dc5da00e7c1a4b0c2a2
Stefan Chen <zchen158@my.bcit.ca>
2022-06-24 17:49:04 -0700

    Refactor by arguments destructuring

--- tutorial/src/components/Navbar.jsx
+++ tutorial/src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 
-function Navbar(props) {
+function Navbar({totalCounters}) {
   return (
     <nav className="navbar bg-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-            Navbar <span className="badge bg-pill bg-secondary">{props.totalCounters}</span>
+            Navbar <span className="badge bg-pill bg-secondary">{totalCounters}</span>
           </a>
         </div>
       </nav>
