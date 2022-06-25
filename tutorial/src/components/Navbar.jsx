12f0e8db9ba364a28cf8ac47c52298eaacda6896
Stefan Chen <zchen158@my.bcit.ca>
2022-06-24 17:43:23 -0700

    Change stateless component from class to function

--- tutorial/src/components/Navbar.jsx
+++ tutorial/src/components/Navbar.jsx
@@ -1,17 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar bg-light">
+
+function Navbar(props) {
+  return (
+    <nav className="navbar bg-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-            Navbar <span className="badge bg-pill bg-secondary">{this.props.totalCounters}</span>
+            Navbar <span className="badge bg-pill bg-secondary">{props.totalCounters}</span>
           </a>
         </div>
       </nav>
-    );
-  }
+  )
 }
 
-export default Navbar;
+export default Navbar
