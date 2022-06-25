990aac58f321b0b4bff1f09945a50ee7de7ff727
Stefan Chen <zchen158@my.bcit.ca>
2022-06-24 17:23:42 -0700

    Create a navbar component

---
+++ tutorial/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Counter from './components/Counter';
-import Counters from './components/Counters'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Counters />
+    <App />
   </React.StrictMode>
 );
   
